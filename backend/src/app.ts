ce81d45b6498c77fc372a3b6433fb542cb094b6a
hijuliancode <hijuliansosa@protonmail.com>
2021-05-25 02:55:00 -0500

    backend: add cors, express.json, express.urlencoded

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan';
+import cors from 'cors'
 import config from './config'
 
 import videoRoutes from "./routes/video.routes";
@@ -9,6 +10,9 @@ const app = express()
 app.set('port', config.PORT)
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.use(videoRoutes)
 
