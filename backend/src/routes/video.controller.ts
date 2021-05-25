8e13c8436fda82f28c54b0646b47060f07c34937
hijuliancode <hijuliansosa@protonmail.com>
2021-05-25 03:30:00 -0500

    backend: POST: Save video on mongodb

--- backend/src/routes/video.controller.ts
+++ backend/src/routes/video.controller.ts
@@ -1,7 +1,17 @@
 import { RequestHandler } from 'express'
+import Video from './Video'
 
-export const createVideo:RequestHandler = (req, res) => {
-  res.json('create video')
+export const createVideo:RequestHandler = async (req, res) => {
+  const videoFound = await Video.findOne({url: req.body.url})
+  if (videoFound)
+    return res.status(301).json({ message: 'The URL already exists' })
+
+  const newVideo = new Video(req.body)
+  const savedVideo = await newVideo.save()
+  res.json({
+    message: 'Video saved',
+    data: savedVideo
+  })
 }
 
 export const getVideos:RequestHandler = (req, res) => {
@@ -17,5 +27,5 @@ export const updateVideo:RequestHandler = (req, res) => {
 }
 
 export const deleteVideo:RequestHandler = (req, res) => {
-  res.json('getting videos')
+  res.json('deleting video')
 }
