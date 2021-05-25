90b217143fc1b7b221ddf682f099d59b4213315d
hijuliancode <hijuliansosa@protonmail.com>
2021-05-25 02:53:01 -0500

    backend: use morgan to see calls to api on terminal

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import morgan from 'morgan';
 import config from './config'
 
 import videoRoutes from "./routes/video.routes";
@@ -6,6 +7,9 @@ import videoRoutes from "./routes/video.routes";
 const app = express()
 
 app.set('port', config.PORT)
+
+app.use(morgan('dev'))
+
 app.use(videoRoutes)
 
 export default app
