935dadb04344ded586ae8be8319293d369ba490e
hijuliancode <hijuliansosa@protonmail.com>
2021-05-25 02:51:32 -0500

    backend: use routes on app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import config from './config'
 
+import videoRoutes from "./routes/video.routes";
+
 const app = express()
 
 app.set('port', config.PORT)
+app.use(videoRoutes)
 
 export default app
